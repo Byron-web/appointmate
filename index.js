7daa6552039e182a917c5c3cf45952cd9d253139
Byron-web <byron.labuschagne@gmail.com>
2023-05-11 16:40:33 +0200

    Heroku changes

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || config.port;
 
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+//Serve static files if app is in production mode.
+if (process.env.NODE_ENV === "production") {
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// All remaining requests return the React app, so it can handle routing.
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build"));
-});
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build"));
+  });
+}
 
 //The Routes
 app.use("/api/users", userRoutes);
